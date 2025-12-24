1f07bfed8c0a99e630ad92e67f9c686c43c59db2
minahilhasan <minahilhasan@gmail.com>
2025-12-24 23:19:26 +0500

    Update stt.py

--- stt.py
+++ stt.py
@@ -44,7 +44,7 @@ def give_response(text):
     try:
         audio = elevenlabs.text_to_speech.convert(
             text=text,
-            voice_id="JBFqnCBsd6RMkjVDRZzb",
+            voice_id="MNInd0RnQzEE7CRejaOV",
             model_id="eleven_multilingual_v2",
             output_format="mp3_44100_128",
         )
