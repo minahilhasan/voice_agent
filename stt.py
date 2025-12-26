71338d45cfd2d5c20e3e9305b23d3b0385072696
minahilhasan <minahilhasan@gmail.com>
2025-12-26 18:52:29 +0400

    Update stt.py

--- stt.py
+++ stt.py
@@ -27,7 +27,7 @@ def aireply(transcription):
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=messages,
-        max-token=450
+        max-token=250
     )
 
     reply = response.choices[0].message.content
